3db78cab7bac0231aab158d4b809f9cf8ca3cb39
Can Kırşallıoba <cankirsallioba@gmail.com>
2020-08-25 19:56:52 +0300

    added some functionality

--- scripts.js
+++ scripts.js
@@ -7,7 +7,7 @@ let fillColor = "brush";
 let lineWidth = 2;
 let polygoneSides = 6;
 let currentTool = 'brush';
-let canvasWidth = 600;
+let canvasWidth = 1000;
 let canvasHeight = 600;
 
 class ShapeBoundingBox {
@@ -17,4 +17,38 @@ class ShapeBoundingBox {
         this.width = width;
         this.height = height;
     }
+}
+
+class MouseDownPos {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+class PolygonPoint {
+    constructor(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+}
+
+let shapeBoundingBox = new ShapeBoundingBox(0,0,0,0);
+let mouseDown = new MouseDownPos(0,0);
+let loc = new Location(0,0);
+
+document.addEventListener('DOMContentLoaded', setupCanvas);
+
+function setupCanvas() {
+    canvas = document.getElementById('my-canvas');
+    ctx = canvas.getContext('2d');
+    ctx.strokeStyle = strokeColor;
+    ctx.lineWidth = lineWidth;
+    canvas.addEventListener("mousedown", ReactToMouseDown);
+    canvas.addEventListener("mousemove", ReactToMouseMove);
+    canvas.addEventListener("mouseup", ReactToMouseUp)
+}
+
+function ChangeTool(toolClicked) {
+    
 }
