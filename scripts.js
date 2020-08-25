1a5f74ed3406b40c45ecb1101261a3ba1f97b42c
Can Kırşallıoba <cankirsallioba@gmail.com>
2020-08-25 20:28:29 +0300

    bug fix

--- scripts.js
+++ scripts.js
@@ -34,7 +34,7 @@ class PolygonPoint {
 }
 
 let shapeBoundingBox = new ShapeBoundingBox(0,0,0,0);
-let mouseDown = new MouseDownPos(0,0);
+let mousedown = new MouseDownPos(0,0);
 let loc = new Location(0,0);
 
 document.addEventListener('DOMContentLoaded', setupCanvas);
@@ -67,4 +67,75 @@ function GetMousePosition(x,y) {
         y: (y - canvasSize.Data.top) * (canvas.height / canvasSizeData.height)};
 }
 
-function SaveCanvasImage
+function SaveCanvasImage() {
+    savedImageData = ctx.getImageData(0, 0, canvas.width,canvas.height);
+}
+
+function RedrawCanvasImage() {
+    ctx.putImageData(savedImageData,0,0);
+}
+
+function UpdateRubberbandSizeData(loc){
+    shapeBoundingBox.width = Math.abs(loc.x - mousedown.x);
+    shapeBoundingBox.height = Math.abs(loc.y - mousedown.y);
+
+    if(loc.x > mousedown.x){
+        shapeBoundingBox.left = mousedown.x;
+    } else {
+        shapeBoundingBox.left = loc.x;
+    }
+
+    if(loc.y > mousedown.y){
+        shapeBoundingBox.top = mousedown.y;
+    } else {
+        shapeBoundingBox.top = loc.y;
+    }
+}
+
+function getAngleUsingXAndY(mouselocX, mouselocY){
+    let adjacent = mousedown.x - mouselocX;
+    let opposite = mousedown.y - mouselocY;
+
+    return radiansToDegrees(Math.atan2(opposite, adjacent));
+}
+
+function radiansToDegrees(rad){
+    if (rad < 0){
+        return (360.0 + (rad * (180 / Math.PI))).toFixed(2);
+    } else {
+        return (rad * (180 / Math.PI)).toFixed(2);
+    }
+}
+
+function degreesToRadians(degrees){
+    return degrees * (Math.PI / 180);
+}
+
+function ReactToMouseDown(e){
+    canvas.style.cursor = "crosshair";
+    loc = GetMousePosition(e.clientX, e.clientY);
+    SaveCanvasImage();
+    mousedown.x = loc.x;
+    mousedown.y = loc.y;
+    dragging = true;
+}
+
+function ReactToMouseMove(e){
+    canvas.style.cursor = "crosshair";
+    loc = GetMousePosition(e.clientX, e.clientY);
+}
+
+function ReactToMouseUp(e){
+    canvas.style.cursor = "default";
+    loc = GetMousePosition(e.clientX, e.clientY);
+    RedrawCanvasImage();
+    UpdateRubberbandOnMove(loc);
+    dragging = false;
+    usingBrush = false;
+}
+
+function SaveImage(){
+    var imageFile = document.getElementById("img-file");
+    imageFile.setAttribute('download', 'image.png');
+    imageFile.setAttribute('href', canvas.toDataURL());
+}
